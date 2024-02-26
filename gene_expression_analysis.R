c174be539882061e5a09106d6b4c128c10c01fbd
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-26 11:38:24 +0100

    Choosing the correct columns

--- gene_expression_analysis.R
+++ gene_expression_analysis.R
@@ -13,3 +13,24 @@ library(dplyr)
 library(tidyverse)
 #install.packages("dslabs")
 library (dslabs)
+
+#take columns you need
+TLS_signature <- 
+  c("CD79A", "FCRL5", "SSR4", "XBP1", 
+    "IL7R", "CXCL12", "LUM", "C1QA", "C7",
+    "CD52", "APOE", "PTGDS", "PIM2", "DERL3",
+    "CCL19", "CCL21", "CXCL13", "CCL17", "CCL22", 
+    "IL16", "ICAM2", "ICAM3", "VCAM1", "MADCAM1", 
+    "ITGAL", "ITGA4", "ITGAD", "LTB",  "CD37")
+
+data <- subset(data_mrna_seq_v2_rsem, Hugo_Symbol %in% TLS_signature)
+View(data)
+
+#put correct names and eliminate the columns which you don't need, transform
+rownames(data) <- data[,1]
+dat <- 
+  data[, -which(colnames(data) == "Hugo_Symbol")] 
+dat <-
+  dat[, -which(colnames(dat)== "Entrez_Gene_Id")]
+dat <- log2(dat + 1)
+
