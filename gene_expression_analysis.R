ba228a213054bdb6c9809c7a8af6ceebf0e76e48
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-26 11:36:13 +0100

    Open file

--- gene_expression_analysis.R
+++ gene_expression_analysis.R
@@ -1,1 +1,5 @@
-#First git
+#### OPEN FILE AND PREPARE IT ####
+
+data_mrna_seq_v2_rsem <- 
+  read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/Raw_data/luad_tcga_pan_can_atlas_2018/data_mrna_seq_v2_rsem.txt")
+View(data_mrna_seq_v2_rsem)
