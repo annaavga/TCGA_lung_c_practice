7f36aee40d3b90510eadfefc270bfb533b66f53f
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-26 11:37:10 +0100

    Libraries

--- gene_expression_analysis.R
+++ gene_expression_analysis.R
@@ -3,3 +3,13 @@
 data_mrna_seq_v2_rsem <- 
   read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/Raw_data/luad_tcga_pan_can_atlas_2018/data_mrna_seq_v2_rsem.txt")
 View(data_mrna_seq_v2_rsem)
+
+###libraries
+#install.packages("ggplot2")
+library(ggplot2)
+#install.packages("dplyr")
+library(dplyr)
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("dslabs")
+library (dslabs)
