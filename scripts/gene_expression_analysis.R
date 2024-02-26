abc0dfe5c202c50181aebb6a7b31a308d4299204
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-26 14:44:06 +0100

    New location of data

--- scripts/gene_expression_analysis.R
+++ scripts/gene_expression_analysis.R
@@ -1,7 +1,7 @@
 #### OPEN FILE AND PREPARE IT ####
 
 data_mrna_seq_v2_rsem <- 
-  read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/Raw_data/luad_tcga_pan_can_atlas_2018/data_mrna_seq_v2_rsem.txt")
+  read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/TCGA_lung_c_practice/raw_data/data_mrna_seq_v2_rsem.txt", header = T)
 View(data_mrna_seq_v2_rsem)
 
 ###libraries
