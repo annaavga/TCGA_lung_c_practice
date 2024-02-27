c5a9f5f71af9069174db90ca934017311a7c3d95
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-27 15:24:49 +0100

    Joining df

--- scripts/analysis_of_both_df.R
+++ scripts/analysis_of_both_df.R
@@ -14,8 +14,22 @@ str(data_clinical_patient)
 data_gene_expression <-
   read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/TCGA_lung_c_practice/processed_data/data_gene_expression.txt", 
              header = T)
+data_clinical <- data_clinical_patient %>% 
+  as.data.frame(str_replace_all(data_clinical_patient$PATIENT_ID,"-",".")) %>%
+  rownames_to_column(var = "PATIENT.ID")
+
+class(data_clinical$PATIENT.ID)
+
+data_clinical$PATIENT.ID <- paste0(data_clinical$PATIENT.ID, ".01")
 
 data_gene_expr <- data_gene_expression %>% t() %>% as.data.frame() %>% rownames_to_column(var = "PATIENT.ID")
+
+#Joining the dataframes of gene expression and clinical data
+merged_data <- data_gene_expr %>% left_join(data_clinical, by= 'PATIENT.ID')
+str(merged_data)
+
+
+
 
 
 
