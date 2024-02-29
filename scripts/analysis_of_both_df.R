d97ad4437d63312cf85933d540b5bc413e111e5b
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-29 12:24:59 +0100

    GSVA method 1st try

--- scripts/analysis_of_both_df.R
+++ scripts/analysis_of_both_df.R
@@ -105,12 +105,54 @@ exp(coef(log_reg_survival))
 # diminishes by 0.3352, significantly (pval<0.05)
 
 #### GSVA METHOD ####
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-library(BiocManager)
 
-BiocManager::install("GSVA")
-library(boot, cluster, foreign, lattice, MASS, Matrix, mgcv, nlme, survival)
+data_mrna <- as.matrix(read.delim(
+  "C:/Users/FixeI/OneDrive/Bureau/Anna/R/TCGA_lung_c_practice/raw_data/data_mrna_seq_v2_rsem.txt", 
+  header = T))
 
+rownames(data_mrna) <- data_mrna[,1]
+data_mrna <- 
+  data_mrna[, -which(colnames(data_mrna) == "Hugo_Symbol")] 
+data_mrna <-
+  data_mrna[, -which(colnames(data_mrna)== "Entrez_Gene_Id")]
+#data_mrna <- log2(data_mrna + 1)
+
+
+class(data_mrna) <- "numeric"
+data_mrna <- data_mrna[complete.cases(data_mrna), ]
+
+gene_names <- read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/TCGA_lung_c_practice/raw_data/data_mrna_seq_v2_rsem.txt", 
+)
+gene_names <- select(gene_names$Hugo_Symbol)
+name <- gene_names %>% 
+  select_if(is.character) 
+class(name) <- "character"
+
+TLS_signature <- 
+  c("CD79A", "FCRL5", "SSR4", "XBP1", 
+    "IL7R", "CXCL12", "LUM", "C1QA", "C7",
+    "CD52", "APOE", "PTGDS", "PIM2", "DERL3",
+    "CCL19", "CCL21", "CXCL13", "CCL17", "CCL22", 
+    "IL16", "ICAM2", "ICAM3", "VCAM1", "MADCAM1", 
+    "ITGAL", "ITGA4", "ITGAD", "LTB",  "CD37")
+
+TLS_signature <- as.list(TLS_signature)
+class(TLS_signature)
+TLS_NAMES <- as.character(TLS_signature)
+
+gsvaPar <- gsvaParam(data_mrna, TLS_signature)
+gsvaPar
+
+gsva.es <- gsva(gsvaPar, verbose=F, annotation = name)
+dim(gsva.es)
+
+
+?gsva
+
+
+dup <- data.frame(as.numeric(duplicated(data$X))) #creates df with binary var for duplicated rows 
+colnames(dup) <- c("dup") #renames column for simplicity 
+df2 <- cbind(data, dup) #bind to original df 
+df3 <- subset(df2, dup == 1) #subsets df using binary var for duplicated`
 
 
