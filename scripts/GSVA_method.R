d3715db2e8b34e1228786f9a1fff9c12bc03659d
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-02-29 15:00:59 +0100

    Perform GSVA

--- scripts/GSVA_method.R
+++ scripts/GSVA_method.R
@@ -4,34 +4,46 @@ source("./scripts/Packages.R")
 source("./scripts/Functions.R")
 
 # load data
-data_mrna <- as.matrix(read.delim(
+data_mrna <- read.delim(
   "C:/Users/FixeI/OneDrive/Bureau/Anna/R/TCGA_lung_c_practice/raw_data/data_mrna_seq_v2_rsem.txt", 
-  header = T))
+  header = T)
+#choose data from gene set
+TLS_signature <- 
+  c("CD79A", "FCRL5", "SSR4", "XBP1", 
+    "IL7R", "CXCL12", "LUM", "C1QA", "C7",
+    "CD52", "APOE", "PTGDS", "PIM2", "DERL3",
+    "CCL19", "CCL21", "CXCL13", "CCL17", "CCL22", 
+    "IL16", "ICAM2", "ICAM3", "VCAM1", "MADCAM1", 
+    "ITGAL", "ITGA4", "ITGAD", "LTB",  "CD37")
 
-rownames(data_mrna) <- data_mrna[,1]
-data_mrna <- 
-  data_mrna[, -which(colnames(data_mrna) == "Hugo_Symbol")] 
-data_mrna <-
-  data_mrna[, -which(colnames(data_mrna)== "Entrez_Gene_Id")]
-#data_mrna <- log2(data_mrna + 1)
+data_gene_set <- subset(data_mrna, Hugo_Symbol %in% TLS_signature)
+rownames(data_gene_set) <- data_gene_set[,1]
+data_gene_set <- 
+  data_gene_set[, -which(colnames(data_gene_set) == "Hugo_Symbol")] 
+data_gene_set <-
+  data_gene_set[, -which(colnames(data_gene_set)== "Entrez_Gene_Id")]
+data_gene_set <- log2(data_gene_set + 1)
 
+#take sample of genes, without those included in the dataset
+data_no_set <- data_mrna[!(data_mrna$Hugo_Symbol %in% TLS_signature),]
+data_sampled <- sample_n(data_no_set, 100)
 
-class(data_mrna) <- "numeric"
-data_mrna <- data_mrna[complete.cases(data_mrna), ]
+rownames(data_sampled) <- data_sampled[,1]
+data_sampled <- 
+  data_sampled[, -which(colnames(data_sampled) == "Hugo_Symbol")] 
+data_sampled <-
+  data_sampled[, -which(colnames(data_sampled)== "Entrez_Gene_Id")]
+data_sampled <- log2(data_sampled + 1)
 
-?complete.cases
+#join the two dataframes
+finaldat_GSVA <- data_sampled %>% bind_rows(data_gene_set)
+str(finaldat_GSVA)
 
+#prepare the df to do the analysis
+finaldat_GSVA <- as.matrix(finaldat_GSVA)
+class(finaldat_GSVA) <- "numeric"
 
-
-
-
-gene_names <- read.delim("C:/Users/FixeI/OneDrive/Bureau/Anna/R/TCGA_lung_c_practice/raw_data/data_mrna_seq_v2_rsem.txt", 
-)
-gene_names <- select(gene_names$Hugo_Symbol)
-name <- gene_names %>% 
-  select_if(is.character) 
-class(name) <- "character"
-
+#choose the gene signature
 TLS_signature <- 
   c("CD79A", "FCRL5", "SSR4", "XBP1", 
     "IL7R", "CXCL12", "LUM", "C1QA", "C7",
@@ -42,19 +54,23 @@ TLS_signature <-
 
 TLS_signature <- as.list(TLS_signature)
 class(TLS_signature)
-TLS_NAMES <- as.character(TLS_signature)
 
-gsvaPar <- gsvaParam(data_mrna, TLS_signature)
+#PERFORM GSVA
+gsvaPar <- gsvaParam(finaldat_GSVA , TLS_signature)
 gsvaPar
 
-gsva.es <- gsva(gsvaPar, verbose=F, annotation = name)
-dim(gsva.es)
+gsva.es <- gsva(gsvaPar, verbose=F)
+#dim(gsva.es)
 
+gsva.es <- as.data.frame(gsva.es)
+class(gsva.es)
 
-?gsva
+#add the names of the genes into the dataframe
+names <- as.data.frame(data_mrna$Hugo_Symbol)
+names <- subset(names, data_mrna$Hugo_Symbol %in% TLS_signature)
 
+data_gsva <- cbind(gsva.es, names$`data_mrna$Hugo_Symbol`)
 
-dup <- data.frame(as.numeric(duplicated(data$X))) #creates df with binary var for duplicated rows 
-colnames(dup) <- c("dup") #renames column for simplicity 
-df2 <- cbind(data, dup) #bind to original df 
-df3 <- subset(df2, dup == 1) #subsets df using binary var for duplicated`
+data_gsva <- data_gsva %>% 
+  rename("GENE_NAME" = "names$`data_mrna$Hugo_Symbol`")
+
