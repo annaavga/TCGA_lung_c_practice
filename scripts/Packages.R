7f8b773bc322c8289c0a07a09c0b58e97b37bf52
Anna_AG <anna.avila_galvez@etu.sorbonne-universite.fr>
2024-03-05 15:00:54 +0100

    Clean script

--- scripts/Packages.R
+++ scripts/Packages.R
@@ -19,10 +19,7 @@ library(stringr)
 library(survminer)
 #install.packages("ggpubr")
 library(ggpubr)
-
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
+#install.packages("BiocManager")
 library(BiocManager)
-
 BiocManager::install("GSVA")
 library(GSVA)
